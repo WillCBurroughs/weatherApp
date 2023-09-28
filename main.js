be75563b185df9f2f1a8472a51c49ee7d658c429
William Burroughs <williamburroughs@Williams-MacBook-Pro.local>
2023-09-27 20:38:17 -0400

    Can now submit zip for weather

--- main.js
+++ main.js
@@ -8,14 +8,26 @@ inputZip.type = "number"
 
 document.body.appendChild(inputZip);
 
+// Adding button to submit 
+let submitZip = document.createElement("button");
+submitZip.addEventListener("click", function(){
+    loadIn(inputZip.value);
+});
+submitZip.textContent = "Submit Zip";
+
+document.body.appendChild(submitZip);
 
 // Useful for getting zip
 let apiKey = "77abea49302d916cfd2aab05e9372441";
 let urlVal = "https://api.openweathermap.org/data/2.5/weather?zip=42167&appid=" + apiKey;
 
-async function loadIn(){
+// Will pass in value to this that will be looked up
+async function loadIn(zipVal){
 
-    let responseDate = await fetch(urlVal)
+    // Will return passed in val
+    urlVal = "https://api.openweathermap.org/data/2.5/weather?zip=" + zipVal + "&appid=" + apiKey;
+
+    let responseDate = await fetch(urlVal);
 
     let dataDisplay = await responseDate.json(); 
 
@@ -25,4 +37,7 @@ async function loadIn(){
 
 }
 
-loadIn();
+loadIn(40502);
+
+// Adding ability to call loadIn based on clicking zip code
+
